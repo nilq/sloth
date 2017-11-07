f7f0c1f190959f415131161e3ac2c714f7305bb0
nilq <i.am@nilq.dk>
2017-11-07 22:42:11 +0100

    Add lexer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod sloth;
 
+use sloth::*;
+
 fn main() {
+    let test = r#"
+fib := {
+    |0|
+    |1|
+    |a| (fib a - 1) + fib a - 2
+}
+    "#;
     
+    for t in lexer(&mut test.chars()) {
+        println!("{:#?}", t);
+    }
 }
